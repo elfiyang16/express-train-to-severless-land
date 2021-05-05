bb30e65c8936e46fd9d9b9eddd846a8228e10da1
Elfi_Y <elfiyang16@gmail.com>
2021-05-05 15:09:14 +0100

    feat: replace db instance with documentClient to marshall data

---
+++ js4/getUserData.js
@@ -0,0 +1,33 @@
+"use strict";
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "eu-west-1" });
+
+exports.handler = function (event, context, callback) {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+  const documentClient = new AWS.DynamoDB.documentClient({
+    // will marshall/unmarshall object
+    region: "eu-west-1",
+  });
+  const params = {
+    TableName: "Users",
+    Key: {
+      //   id: {
+      //     S: "12345",
+      //   },
+      id: "12345",
+    },
+  };
+
+  //   ddb.getItem(params, (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     console.log(data);
+  //   });
+  documentClient.getItem(params, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(data);
+  });
+};
