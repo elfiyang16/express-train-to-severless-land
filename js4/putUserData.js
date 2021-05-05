3ce6169431f98091bb95169285483fd6923ab0d1
Elfi_Y <elfiyang16@gmail.com>
2021-05-05 15:27:44 +0100

    feat: put Item

--- js4/putUserData.js
+++ js4/putUserData.js
@@ -5,27 +5,19 @@ AWS.config.update({ region: "eu-west-1" });
 exports.handler = async (event, context) => {
   const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
   const documentClient = new AWS.DynamoDB.DocumentClient({
-    // will marshall/unmarshall object
     region: "eu-west-1",
   });
   const params = {
     TableName: "Users",
-    Key: {
-      //   id: {
-      //     S: "12345",
-      //   },
-      id: "5678",
+    Item: {
+      id: "12345",
+      firstname: "Julia",
+      lastname: "Mason",
     },
   };
 
-  //   ddb.getItem(params, (err, data) => {
-  //     if (err) {
-  //       console.log(err);
-  //     }
-  //     console.log(data);
-  //   });
   try {
-    const data = await documentClient.get(params).promise();
+    const data = await documentClient.put(params).promise();
     console.log(data);
   } catch (err) {
     console.log(err);
